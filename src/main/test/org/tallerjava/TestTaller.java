790cee19a421552d250195ee5ef6604bd6126e35
Luquiprogrammer <lucas.brito@estudiantes.utec.edu.uy>
2026-05-21 11:37:00 -0300

    Archivo de test nuevo

--- src/main/test/org/tallerjava/TestTaller.java
+++ src/main/test/org/tallerjava/TestTaller.java
@@ -1,4 +1,4 @@
-package org.tallerjava.moduloPeaje.aplicacion.impl;
+package org.tallerjava;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.spi.Bean;
@@ -6,20 +6,13 @@ import org.jboss.weld.junit.MockBean;
 import org.jboss.weld.junit5.EnableWeld;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldSetup;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.tallerjava.moduloGestion.interfase.local.ServicioPagoFacade;
-import org.tallerjava.moduloPeaje.dominio.*;
-import org.tallerjava.moduloPeaje.dominio.repo.PeajeRepositorio;
-import org.tallerjava.moduloPeaje.infraestructura.persistencia.PeajeRepositorioImpl;
-import org.tallerjava.moduloPeaje.interfase.evento.out.PublicadorEvento;
 import org.tallerjava.moduloCargas.aplicacion.ServicioCarga;
 import org.tallerjava.moduloCargas.aplicacion.impl.ServicioCargaImpl;
-import org.tallerjava.moduloCargas.dominio.Carga;
-import org.tallerjava.moduloCargas.dominio.Cargador;
 import org.tallerjava.moduloCargas.dominio.EstacionCarga;
 import org.tallerjava.moduloCargas.dominio.repo.CargasRepositorio;
+import org.tallerjava.moduloCargas.interfase.evento.out.PublicadorEventoCarga;
 import org.tallerjava.moduloCargas.test.CargasRepositorioFake;
 import org.tallerjava.moduloClientes.aplicacion.ServicioClientes;
 import org.tallerjava.moduloClientes.dominio.*;
@@ -27,50 +20,47 @@ import org.tallerjava.moduloClientes.aplicacion.impl.ServicioClientesImpl;
 import org.tallerjava.moduloClientes.dominio.repo.ClientesRepositorio;
 import org.tallerjava.moduloClientes.interfase.evento.out.PublicadorEventoCliente;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @EnableWeld
-class VerificoTagExtranjeroConPrePagoOk {
+class TestTaller {
 
     /**
      * Como funcionan los test de la capa de aplicación.
      * Cada implementación (en este caso ServicioPeajeImpl) tiene sus dependencias
-     * las cuales son injectadas por el contender (ejemplo: repositorio, serviciosPagodFacade, publicadorEventos)
+     * las cuales son injectadas por el contender (ejemplo: repositorio,
+     * serviciosPagodFacade, publicadorEventos)
      *
-     * El objetivo de cada test es probar el código del caso de uso, no el código de sus dependencias, por lo tanto,
-     * cuando ejecuto los test tengo que brindar objetos mock (fakes) que devuelvan lo que yo quiero.
+     * El objetivo de cada test es probar el código del caso de uso, no el código de
+     * sus dependencias, por lo tanto,
+     * cuando ejecuto los test tengo que brindar objetos mock (fakes) que devuelvan
+     * lo que yo quiero.
      *
      * Lo primero es configurar Weld para que use dichos objetos fakes.
      *
-     * NOTA: hacer test lleva tiempo, probar todos los casos es deseable en un proyecto real
-     * pero es una activad que consume muchos recursos, por lo que en este instancia, solo haremos los principales
+     * NOTA: hacer test lleva tiempo, probar todos los casos es deseable en un
+     * proyecto real
+     * pero es una activad que consume muchos recursos, por lo que en este
+     * instancia, solo haremos los principales
      *
      */
     @WeldSetup
-    public WeldInitiator weld =
-            WeldInitiator.from(ServicioPeajeImpl.class, ServicioClientesImpl.class, ServicioCargaImpl.class)
-                    .addBeans(crearMockRepositorioImpl())
-                    .addBeans(crearMockServiciosPagosFacade())
-                    .addBeans(crearMockPublicadorEvento())
-                    .addBeans(crearMockClientesRepositorio())
-                    .addBeans(crearMockPublicadorEventoCliente())
-                    .addBeans(crearMockCargaRepositorio())
-                    .build();
-
+    public WeldInitiator weld = WeldInitiator
+            .from( ServicioClientesImpl.class, ServicioCargaImpl.class)
+            .addBeans(crearMockClientesRepositorio())
+            .addBeans(crearMockPublicadorEventoCliente())
+            .addBeans(crearMockCargaRepositorio())
+            .addBeans(crearMockPublicadorEventoCarga())
+            .build();
 
     /**
-     * Esta es la manera en que el framework de dependencias y junit necesita que construyamos los
+     * Esta es la manera en que el framework de dependencias y junit necesita que
+     * construyamos los
      * mock para cada dependencia
+     * 
      * @return
      */
-    private Bean<?> crearMockRepositorioImpl() {
-        return MockBean.builder()
-                .types(PeajeRepositorio.class) //esto lo saco del @inject de ServicioPeajeImpl
-                .scope(ApplicationScoped.class)
-                .creating(crearRepoImpl())  //aca construyo la implementación que será usasa en este test
-                .build();
-    }
+   
 
     private Bean<?> crearMockClientesRepositorio() {
         return MockBean.builder()
@@ -88,7 +78,7 @@ class VerificoTagExtranjeroConPrePagoOk {
                 .scope(ApplicationScoped.class)
                 .creating(new PublicadorEventoCliente() {
                     @Override
-                    public void publicarNuevoCliente(Cliente cliente, double descuento) {
+                    public void publicarNuevoCliente(Cliente cliente) {
                         System.out.println("Disparo nuevo cliente para que otros modulos lo detecten con observadores");
                     }
 
@@ -115,28 +105,19 @@ class VerificoTagExtranjeroConPrePagoOk {
                 .build();
     }
 
-    private Bean<?> crearMockServiciosPagosFacade() {
+
+    private Bean<?> crearMockPublicadorEventoCarga(){
         return MockBean.builder()
-                .types(ServicioPagoFacade.class)
-                .scope(ApplicationScoped.class)
-                .creating(
-                        crearServicioPagoFacade()
-                ).build();
+        .types(PublicadorEventoCarga.class)
+        .scope(ApplicationScoped.class)
+        .creating(new PublicadorEventoCarga())
+        .build();
     }
-
-    private Bean<?> crearMockPublicadorEvento() {
-        return MockBean.builder()
-                .types(PublicadorEvento.class)
-                .scope(ApplicationScoped.class)
-                .creating(
-                        new PublicadorEvento()
-                ).build();
-    }
-
-
     /**
-     * Aca construyo un implementación "fake" del repositorio que me devuelve un datos concreto
+     * Aca construyo un implementación "fake" del repositorio que me devuelve un
+     * datos concreto
      * relevante para ejecutar este test
+     * 
      * @return
      */
 
@@ -172,66 +153,26 @@ class VerificoTagExtranjeroConPrePagoOk {
             @Override
             public void guardarEstacion(EstacionCarga estacionCarga) {
                 System.out.println("Guardando estacion");
+                System.out.println(estacionCarga.getDescripcion() + " " + estacionCarga.getDepartamento());
             }
 
-        };
-    }
-
-    private PeajeRepositorio crearRepoImpl() {
-        return new PeajeRepositorioImpl() {
-
-            @Override
-            public Vehiculo findByTag(int tag) {
-                Vehiculo vehiculo = new Vehiculo(1,
-                        new Identificador("BAA 1111", tag),
-                        "ford", "fiesta", Nacionalidad.EXTRANJERO);
-                return vehiculo;
-            }
-
-
-            @Override
-            public Preferencial obtenerTarifaPreferencial() {
-                return new Preferencial();
-            }
-
-            @Override
-            public Comun obtenerTarifaComun() {
-                return new Comun();
-            }
         };
     }
 
     /**
-     * Implementación "fake" del servicio de pagos, solo implemento este metodo porque es lo
+     * Implementación "fake" del servicio de pagos, solo implemento este metodo
+     * porque es lo
      * unico que preciso para probar este caso de pruebas.
+     * 
      * @return
      *
      */
-    private ServicioPagoFacade crearServicioPagoFacade() {
-        return new ServicioPagoFacade() {
-            public boolean realizarPrePago(int tag, double importe) {
-                //Estoy provando que el pago es aceptdo, por eso retorno true.
-                //Eventualmente debería de crear otro test para probar los casos donde se devuelve false
-                return true;
-            }
-        };
-    }
-
-    @Test
-    @DisplayName("Verifico tag de v. extranjero con PrePago confirmado ok")
-    void estaHabilitadoAutoExtranjeroConTagPrePagoOk(ServicioPeajeImpl servicioPeaje) {
-        Assertions.assertTrue(
-                //obervese que los valores pasados como parámetros tiene poca relevancia
-                //en este test, ya que el vehiculo recuperado desde el fake de repositorio
-                //(implementado arriba) siempre devolverá un vehiculo con tag hardcoded.
-                servicioPeaje.estaHabilitadoSincronico(10001,"BAA 1111"));
-    }
-
+    
     @Test
     @DisplayName("Verifico usuario guardado")
     void testearCliente(ServicioClientes servicioClientes) {
         Cliente cliente = new ClienteComun();
-                servicioClientes.registrarCliente(cliente);
+        servicioClientes.registrarCliente(cliente);
     }
 
     @Test
