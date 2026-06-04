f677a36e0863504ce7337712422eccad61c1fc89
itsmafiu <matthewfreire2005@gmail.com>
2026-06-04 16:06:52 -0300

    RateLimiter probado y andando

--- src/main/java/org/tallerjava/moduloCargas/infraestructura/ratelimiter/RateLimiter.java
+++ src/main/java/org/tallerjava/moduloCargas/infraestructura/ratelimiter/RateLimiter.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 public class RateLimiter {
     private Bucket bucket;
     @Getter
-    private boolean activo;
+    private boolean activo = true;
 
     @PostConstruct
     public void inicializar() {
@@ -20,8 +20,8 @@ public class RateLimiter {
         // cada vez que llega un request, se quita un elemento del balde
         // si el balde se queda vacío los request serán rechazados
         Bandwidth bucketConf = Bandwidth.builder() //config del balde
-                .capacity(10) //capacidad inicial
-                .refillGreedy(5, Duration.ofSeconds(1))
+                .capacity(20) //capacidad inicial
+                .refillGreedy(10, Duration.ofSeconds(1))
         // refillGreedy(cuantos tokens intentará llenar, en qué lapso de tiempo) intentando distribuir el llenado de forma regular
         // ej: refillGreedy(5, Duration.ofSeconds(1)) = token se creara cada 200 milisegundos (1/5=0,200)
         // otra opción = .refillIntervally(5, Duration.ofSeconds(1)) // en lugar de distribuir el relleno, lo hace de golpe
